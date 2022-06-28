b142d5c690a171258b16d795269895681a113820
Carl Clark <carl@caztec.com.au>
2022-06-28 12:15:40 +1000

    Fixed DeploymentLog folder reference

--- Deployment/WriteDeploymentTimestamp.ps1
+++ Deployment/WriteDeploymentTimestamp.ps1
@@ -1,4 +1,4 @@
 $timestamp = Get-Date -Format o;
 Write-Host($timestamp);
-Add-Content ..\DeploymentOutput\DeploymentLog.txt ($timestamp)
+Add-Content .\DeploymentOutput\DeploymentLog.txt ($timestamp)
 #New-Item -Path ..\DeploymentOutput -Name $timestamp -ItemType "file" -Value $timestamp
